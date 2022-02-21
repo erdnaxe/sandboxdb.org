d788074fe5e78707ffe204d66f785005a2dda5bc
Alexandre Iooss <erdnaxe@crans.org>
2022-02-21 18:20:14 +0100

    Collect NixOS services from tests

--- scripts/utils/nixos_source.py
+++ scripts/utils/nixos_source.py
@@ -3,26 +3,71 @@
 
 import re
 import subprocess
+import json
 
 
-def get_services_store_path() -> [(str, str)]:
+def get_services_store_path() -> [str]:
     """Return services files store paths.
 
-    You need to have previously run: `nix-index -f "<nixos-unstable>"`
+    You need to have nix with nixos-unstable channel.
     """
-    ret, out = subprocess.getstatusoutput(
-        "nix-locate --at-root -r '/lib/systemd/.*\.service$'"
-    )
-    if ret != 0:
-        print("Error while calling nix-locate")
-        return []
-    for line in out.split("\n"):
-        line = line.split()
-        name = line[0].split(".")[0].replace("(", "")
-        file_type = line[-2]
-        path = line[-1]
-        if file_type == "r":
-            yield name, path
+    drv_pattern = re.compile(r"-unit-.*\.service$")
+
+    # Collect all NixOS tests
+    nix_tests = set()
+    tests_path = "/nix/var/nix/profiles/per-user/root/channels/nixos-unstable/nixos/tests/all-tests.nix"
+    with open(tests_path, "r") as f:
+        for line in f.readlines():
+            for s in line.strip().split(" "):
+                if s.startswith("./") and s.endswith(".nix"):
+                    nix_tests.add(s[2:])
+
+    services_drv = set()
+    for nix_path in nix_tests:
+        # Instanciate derivation
+        print(f"Instanciating <nixos-unstable/nixos/tests/{nix_path}>")
+        p = subprocess.run(
+            ["nix-instantiate", f"<nixos-unstable/nixos/tests/{nix_path}>"],
+            capture_output=True,
+        )
+        if p.returncode != 0:
+            print("Error while calling nix-instantiate")
+            continue
+        for drv_path in p.stdout.decode().split("\n")[:-1]:
+            # Explore derivation outputs
+            p = subprocess.run(
+                [
+                    "nix",
+                    "--extra-experimental-features",
+                    "nix-command",
+                    "show-derivation",
+                    "-r",
+                    drv_path,
+                ],
+                capture_output=True,
+            )
+            if p.returncode != 0:
+                print("Error while calling nix show-derivation")
+                continue
+            json_out = json.loads(p.stdout)
+            for drv in json_out.values():
+                path = drv.get("outputs", {}).get("out", {}).get("path")
+                if path and drv_pattern.search(path):
+                    unit_name = path.split("-unit-")[-1]
+                    unit_name.replace("-.service", "@.service")
+
+                    if (
+                        unit_name.startswith("acme-")
+                        or "test.service" in unit_name
+                        or unit_name.startswith("test")
+                        or unit_name.startswith("network-addresses-")
+                    ):
+                        continue  # ignore
+
+                    path = f"{path}/{unit_name}"
+                    services_drv.add(path)
+
+    return services_drv
 
 
 def get_service_from_path(path) -> (str, str):
@@ -36,7 +81,9 @@ def get_service_from_path(path) -> (str, str):
         return "", ""
     service_name = path.split("/")[-1]
 
-    # Remove store paths from output
+    # Remove NixOS specific from output
     out = re.sub(r"/nix/store/[^/]+/", "/usr/", out)
+    out = re.sub(r"Environment=\"(PATH|TZDIR|LOCALE_ARCHIVE)=.+\"\n", "\n", out)
+    out = re.sub(r"X-[\w-]+=.+\n", "\n", out)
 
     return service_name, out
